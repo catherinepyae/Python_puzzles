f134219863d902b57f8f856ec7f07f3fbc5ed4e4
Pyae Phyo Thet <catherinepyae1296@gmail.com>
2023-06-10 00:44:40 +0630

    second commit

---
+++ Basic_Python/Task2.py
@@ -0,0 +1,7 @@
+# Collections
+# next five most important types in Python
+# Strings
+# lists
+# typles
+# dictionaries and
+# sets
---
+++ Basic_Python/Task3.py
@@ -0,0 +1,16 @@
+# Strings(str)
+# sequences of characters and can be used to store text of all kinds.
+# can concatenate strings to create a new string using the plus sign
+# can also access individual characters using square brackets and an index
+# name of class for strings is 'str'
+# can turn other objects into strings
+
+s = "Hello"
+t = "World."
+u = s + t
+print(type(u))
+print(u)
+print(u[9])
+
+n = str(1234)
+print(n[2])
